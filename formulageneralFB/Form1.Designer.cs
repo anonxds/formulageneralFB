67bb53e116adb0dde16e2960266eb3daacc8b9f4
orlandoxds <34320335+orlandoxds@users.noreply.github.com>
2019-01-28 17:10:43 -0800

    h

--- formulageneralFB/Form1.Designer.cs
+++ formulageneralFB/Form1.Designer.cs
@@ -36,81 +36,122 @@
             this.lblx2 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.btnrefresh = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // txta
             // 
-            this.txta.Location = new System.Drawing.Point(118, 104);
+            this.txta.Location = new System.Drawing.Point(67, 44);
+            this.txta.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txta.Name = "txta";
-            this.txta.Size = new System.Drawing.Size(100, 20);
+            this.txta.Size = new System.Drawing.Size(148, 26);
             this.txta.TabIndex = 0;
             // 
             // btnresultado
             // 
-            this.btnresultado.Location = new System.Drawing.Point(118, 192);
+            this.btnresultado.Location = new System.Drawing.Point(37, 187);
+            this.btnresultado.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnresultado.Name = "btnresultado";
-            this.btnresultado.Size = new System.Drawing.Size(75, 23);
+            this.btnresultado.Size = new System.Drawing.Size(112, 35);
             this.btnresultado.TabIndex = 1;
-            this.btnresultado.Text = "button1";
+            this.btnresultado.Text = "Respuesta";
             this.btnresultado.UseVisualStyleBackColor = true;
             this.btnresultado.Click += new System.EventHandler(this.btnresultado_Click);
             // 
             // txtb
             // 
-            this.txtb.Location = new System.Drawing.Point(118, 130);
+            this.txtb.Location = new System.Drawing.Point(67, 84);
+            this.txtb.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtb.Name = "txtb";
-            this.txtb.Size = new System.Drawing.Size(100, 20);
+            this.txtb.Size = new System.Drawing.Size(148, 26);
             this.txtb.TabIndex = 2;
             // 
             // txtc
             // 
-            this.txtc.Location = new System.Drawing.Point(118, 156);
+            this.txtc.Location = new System.Drawing.Point(67, 124);
+            this.txtc.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtc.Name = "txtc";
-            this.txtc.Size = new System.Drawing.Size(100, 20);
+            this.txtc.Size = new System.Drawing.Size(148, 26);
             this.txtc.TabIndex = 3;
             // 
             // lblx1
             // 
             this.lblx1.AutoSize = true;
-            this.lblx1.Location = new System.Drawing.Point(267, 107);
+            this.lblx1.Location = new System.Drawing.Point(260, 47);
+            this.lblx1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblx1.Name = "lblx1";
-            this.lblx1.Size = new System.Drawing.Size(35, 13);
+            this.lblx1.Size = new System.Drawing.Size(13, 20);
             this.lblx1.TabIndex = 4;
-            this.lblx1.Text = "label1";
+            this.lblx1.Text = " ";
             // 
             // lblx2
             // 
             this.lblx2.AutoSize = true;
-            this.lblx2.Location = new System.Drawing.Point(267, 133);
+            this.lblx2.Location = new System.Drawing.Point(260, 87);
+            this.lblx2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblx2.Name = "lblx2";
-            this.lblx2.Size = new System.Drawing.Size(35, 13);
+            this.lblx2.Size = new System.Drawing.Size(13, 20);
             this.lblx2.TabIndex = 5;
-            this.lblx2.Text = "label1";
+            this.lblx2.Text = " ";
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(346, 67);
+            this.dataGridView1.Location = new System.Drawing.Point(264, 122);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(321, 255);
+            this.dataGridView1.Size = new System.Drawing.Size(923, 416);
             this.dataGridView1.TabIndex = 6;
             // 
             // btnrefresh
             // 
-            this.btnrefresh.Location = new System.Drawing.Point(137, 293);
+            this.btnrefresh.Location = new System.Drawing.Point(37, 253);
+            this.btnrefresh.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnrefresh.Name = "btnrefresh";
-            this.btnrefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnrefresh.Size = new System.Drawing.Size(112, 35);
             this.btnrefresh.TabIndex = 7;
-            this.btnrefresh.Text = "button1";
+            this.btnrefresh.Text = "refrescar";
             this.btnrefresh.UseVisualStyleBackColor = true;
             this.btnrefresh.Click += new System.EventHandler(this.btnrefresh_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "B";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "C";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnrefresh);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.lblx2);
@@ -119,6 +160,7 @@
             this.Controls.Add(this.txtb);
             this.Controls.Add(this.btnresultado);
             this.Controls.Add(this.txta);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -138,6 +180,9 @@
         private System.Windows.Forms.Label lblx2;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button btnrefresh;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
